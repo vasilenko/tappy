67a7e184da06022ecb479f9df9dfd62aae9cf4ae
Nikolai Vasilenko <colormailbox@gmail.com>
2016-03-12 17:20:36 +0300

    Rewrite game with P2 Physics

--- js/game.js
+++ js/game.js
@@ -8,41 +8,46 @@ function preload() {
   game.load.image('background', 'assets/background.png');
   game.load.image('ground', 'assets/ground.png');
   game.load.image('rock', 'assets/rock.png');
-
   game.load.spritesheet('plane', 'assets/plane.png', 88, 73);
+  game.load.physics('shapes', 'assets/shapes.json');
 }
 
 function create() {
-  game.physics.startSystem(Phaser.Physics.ARCADE);
+  game.physics.startSystem(Phaser.Physics.P2JS);
+  game.physics.p2.setImpactEvents(true);
+  game.physics.p2.gravity.y = 200;
 
   background = game.add.tileSprite(0, 0, game.world.width, game.world.height, 'background');
-
-  ground = game.add.tileSprite(0, game.world.height - 71, game.world.width, 71, 'ground');
-  game.physics.arcade.enable(ground);
-  ground.body.immovable = true;
-
+  ground = game.add.sprite(404, game.world.height - 35, 'ground');
   plane = game.add.sprite(game.world.centerX, game.world.centerY, 'plane');
-  plane.anchor.setTo(0.5, 0.5);
+
+  game.physics.p2.enable([ground, plane]);
+
+  var groundCollisionGroup = game.physics.p2.createCollisionGroup();
+  var planeCollisionGroup = game.physics.p2.createCollisionGroup();
+
+  ground.body.static = true;
+  ground.body.clearShapes();
+  ground.body.loadPolygon('shapes', 'ground');
+  ground.body.setCollisionGroup(groundCollisionGroup);
+  ground.body.collides(planeCollisionGroup);
+
+  plane.body.clearShapes();
+  plane.body.loadPolygon('shapes', 'plane');
+  plane.body.setCollisionGroup(planeCollisionGroup);
+  plane.body.collides(groundCollisionGroup, function() {
+    plane.kill();
+  });
+
   plane.animations.add('fly', [0, 1, 2, 1], 10, true);
   plane.animations.play('fly');
 
-  game.physics.arcade.enable(plane);
-  plane.body.gravity.y = 200;
-  plane.body.collideWorldBounds = true;
-
   plane.inputEnabled = true;
-  plane.events.onInputDown.add(tap, this)
+  plane.events.onInputDown.add(function() {
+    plane.body.velocity.y = -200;
+  });
 }
 
 function update() {
   background.tilePosition.x -= 0.25;
-  ground.tilePosition.x -= 1;
-
-  game.physics.arcade.collide(plane, ground, function() {
-    plane.kill();
-  });
-}
-
-function tap() {
-  plane.body.velocity.y = -200;
 }
