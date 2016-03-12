6b9455e66f03a9f3a5b0bfeb03624ba3ec7b2037
Nikolai Vasilenko <colormailbox@gmail.com>
2016-03-12 12:55:45 +0300

    Make plane fly

--- js/game.js
+++ js/game.js
@@ -1,20 +1,28 @@
 var game = new Phaser.Game(800, 480, Phaser.AUTO, '', { preload: preload, create: create, update: update }, true);
 
+var background;
+var ground;
+var plane;
+
 function preload() {
   game.load.image('background', 'assets/background.png');
   game.load.image('ground', 'assets/ground.png');
-  game.load.image('plane', 'assets/plane.png');
   game.load.image('rock', 'assets/rock.png');
+
+  game.load.spritesheet('plane', 'assets/plane.png', 88, 73);
 }
 
 function create() {
-  game.add.sprite(0, 0, 'background');
-  game.add.sprite(0, game.world.height - 71, 'ground');
+  background = game.add.tileSprite(0, 0, game.world.width, game.world.height, 'background');
+  ground = game.add.tileSprite(0, game.world.height - 71, game.world.width, 71, 'ground');
 
-  var plane = game.add.sprite(game.world.centerX, game.world.centerY, 'plane');
+  plane = game.add.sprite(game.world.centerX, game.world.centerY, 'plane');
   plane.anchor.setTo(0.5, 0.5);
+  plane.animations.add('fly', [0, 1, 2, 1], 10, true);
+  plane.animations.play('fly');
 }
 
 function update() {
-
+  background.tilePosition.x -= 0.25;
+  ground.tilePosition.x -= 1;
 }
